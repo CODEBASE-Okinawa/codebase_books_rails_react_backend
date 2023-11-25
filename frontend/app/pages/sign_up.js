9b589c7c321426f9e5e1021f3c063c6408651657
zamami <s1413115@gmail.com>
2023-11-25 15:41:28 +0900

    signupのルーティング修正（まだレスポンスは未確認)

--- frontend/app/pages/sign_up.js
+++ frontend/app/pages/sign_up.js
@@ -1,24 +1,39 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-export default function SignIn() {
+export default function SignUp() {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [password_confirmation, setPasswordConfirmation] = useState('');
 
   const handleSignUp = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:3000/api/v1/auth/sign_up', {
+      const response = await axios.post('http://localhost:3000/api/v1/auth', {
+        name: name,
         email: email,
         password: password,
+        password_confirmation: password_confirmation,
       });
 
       // サインイン成功時の処理
       console.log(response.data); // レスポンスの確認
 
+      // 取得したresponseより、アクセストークンなどを変数に代入
+      const accessToken = response.headers['access-token'];
+      const client = response.headers['client'];
+      const uid = response.headers['uid'];
+
+
+      // 認証情報をlocalStorageに保存する
+      localStorage.setItem('access-token', accessToken);
+      localStorage.setItem('client', client);
+      localStorage.setItem('uid', uid);
+
       // リダイレクト
-      window.location.href = 'http://localhost:8000/login';
+      window.location.href = 'http://localhost:8000/sign_in';
     } catch (error) {
       // エラー時の処理
       console.error('サインインエラー:', error.response.data.errors);
@@ -29,6 +44,20 @@ export default function SignIn() {
       <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
         <h1 className="text-3xl font-bold text-center text-gray-700">CODEBASE BOOKS</h1>
         <form className="mt-6" onSubmit={handleSignUp}>
+        <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Name
+            </label>
+            <input
+              type="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
           <div className="mb-4">
             <label
               htmlFor="email"
@@ -57,9 +86,23 @@ export default function SignIn() {
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password confirmation
+            </label>
+            <input
+              type="password"
+              value={password_confirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
           <div className="mt-2">
             <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-              Signin
+              Signup
             </button>
           </div>
         </form>
